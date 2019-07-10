77f84c69bdeeee85a6443e73cbd2145c3034c7d1
Dmitriy Zaitsev <greazz@inbox.ru>
2019-07-10 13:48:48 +0300

    Revert "Performance improvements. Refactoring. No Lists/Array now."
    This reverts commit 644bb5bab983d41e1c91a245beac5ee5f7fd1176.

--- EPAM-Training-2/src/Main.java
+++ EPAM-Training-2/src/Main.java
@@ -85,30 +85,26 @@ public class Main {
         return largestDigit;
     }
 
-    static boolean isPalindrome(int n) {
+    static boolean Palindrome (int n) {
         return n == ReverseNumber(n);
     }
 
-    static boolean isPrimeNumber(int n) {
+    static boolean PrimeNumber (int n) {
         return ProperDivisorsCount(n) == 1;
     }
 
     static void PrimeDivisors(int n) {  // Все простые множители
         if (n > 1) {
-            if (!isPrimeNumber(n)) { // if input number is not prime
-                for (int i = 1; i <= Math.sqrt(n); i++) {
-                    if (n % i == 0 && i != n && n / i != n) {
-                        if (n / i != i && isPrimeNumber(n / i)) { // if divisors are different
-                            System.out.print(n / i + " ");
-                        } else {
-                            if (isPrimeNumber(i)) {
-                                System.out.print(i + " ");
-                            }
+            for (int i = 1; i <= Math.sqrt(n); i++) {
+                if (n % i == 0 && i != n && n / i != n) {
+                    if (n / i != i && PrimeNumber(n / i)) { // if divisors are different
+                        System.out.print(n / i + " ");
+                    } else {
+                        if (PrimeNumber(i)) {
+                            System.out.print(i + " ");
                         }
                     }
                 }
-            } else { // if input number is prime by itself
-                System.out.print(n);
             }
         }
     }
@@ -128,9 +124,9 @@ public class Main {
         return  a * b / GCD(a, b);
     }
 
-    static int UniqueDigits (int n) { // Works fine for any int, cause the only int with 10 same digits (1111111111)
-        int uniqueDigitsCount = 0;    // due specific of the function is determines as 2, which is also consists of
-        long digitsCounts = 0;        // 1 exclusive digit.
+    static int UniqueDigits (int n) {
+        int uniqueDigitsCount = 0;
+        long digitsCounts = 0;
         while (n > 0) {
             digitsCounts += Math.pow(10, n % 10);
             n /= 10;
@@ -144,30 +140,13 @@ public class Main {
         return uniqueDigitsCount;
     }
 
-    // 2nd variant
-    static int UniqueDigitsByShift (long n) { // Works fine for anything integer
-        int uniqueDigitsCount = 0;
-        int digitsCounts = 0;
-        while (n > 0) {
-            digitsCounts |= 0b1 << n % 10;
-            n /= 10;
-        }
-        while (digitsCounts > 0) {
-            if ((digitsCounts & 0b1) == 1) {
-                uniqueDigitsCount++;
-            }
-            digitsCounts >>= 1;
-        }
-        return uniqueDigitsCount;
-    }
-
     /* 3) Натуральное число называют совершенным, если оно равно сумме всех своих
     делителей, не считая только его самого (например, 28=1+2+3+7+14 –
     совершенное число). Напишите программу, которая проверяет, является ли
     введённое натуральное число совершенным. Для проверки работоспособности
     программы приводится список некоторых совершенных чисел: 6, 28, 496, 8128. */
 
-    static boolean isPerfectNumber(int n) {
+    static boolean PerfectNumber (int n) {
         return n == ProperDivisorsSum(n);
     }
 
@@ -193,44 +172,34 @@ public class Main {
     public static void main (String[] args) {
 
         // 1
-        System.out.println("\nTask #1 (Heads & Tales)");
         int iterations = 1000;
         int heads = HeadsOrTales(iterations, true);
         int tales = iterations - heads;
-        System.out.println( heads + " Heads");
-        System.out.println( tales + " Tales" );
-        System.out.println( "out of " + iterations + " iteration(s)" );
+        System.out.println( "Heads: " + heads );
+        System.out.println( "Tales: " + tales );
 
         // 2
-        int inputNumber = 12721;
-        System.out.println( "\nTask #2" );
-        System.out.println( "INPUT:           " + inputNumber );
-        System.out.println( "Largest digit:   " + LargestDigit(inputNumber) );
-        System.out.println( "Palindrome:      " + isPalindrome(inputNumber) );
-        System.out.println( "Prime (простое): " + isPrimeNumber(inputNumber) );
-        System.out.print( "Prime divisors:  " );
-        PrimeDivisors(inputNumber);
+        System.out.println( LargestDigit(12742) );
+        System.out.println( Palindrome(12721) );
+        System.out.println( Palindrome(12742) );
+        System.out.println( PrimeNumber(12742) );
+        PrimeDivisors(12742);
         System.out.println();
-        System.out.println( UniqueDigits(inputNumber) + " exclusive digits" );
-        System.out.println();
-        int GCDInputNumber1 = 1071;
-        int GCDInputNumber2 = 462;
-        System.out.println( "INPUT:           " + GCDInputNumber1 + " " + GCDInputNumber2 );
-        System.out.println( "GCD (НОД):       " + GCD(GCDInputNumber1, GCDInputNumber2) );
-        System.out.println( "GCD (НОК):       " + LCM(GCDInputNumber1, GCDInputNumber2) );
+        System.out.println( GCD(1071, 462) );
+        System.out.println( LCM(16, 20) );
+        System.out.println( UniqueDigits(12742) );
+        System.out.println( UniqueDigits(1111111111) );
 
         // 3
-        System.out.println("\nTask #3 (Совершенное число)");
-        System.out.println( "1:    " + isPerfectNumber(1) );
-        System.out.println( "6:    " + isPerfectNumber(6) );
-        System.out.println( "28:   " + isPerfectNumber(28) );
-        System.out.println( "496:  " + isPerfectNumber(496) );
-        System.out.println( "8128: " + isPerfectNumber(8128) );
+        System.out.println("\nTask #3");
+        System.out.println( PerfectNumber(1) );
+        System.out.println( PerfectNumber(6) );
+        System.out.println( PerfectNumber(28) );
+        System.out.println( PerfectNumber(496) );
+        System.out.println( PerfectNumber(8128) );
 
         // 4
-        int rangeBeg = 0;
-        int rangeEnd = 140000;
-        System.out.println("\nTask #4 (Дружественные числа [в диапазоне " + rangeBeg + " - " + rangeEnd + "])");
-        AmicableNumbersInRange(rangeBeg, rangeEnd);
+        System.out.println("\nTask #4");
+        AmicableNumbersInRange(1, 20000);
     }
 }
